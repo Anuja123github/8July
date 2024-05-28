bcaaca435d0279823e23f161f415a2ccec0cc9a9
Anuja <anujakamble@vctpune.co.in>
2024-05-28 15:14:11 +0530

    added arrayList code

---
+++ SpringDemo/src/com/velocity/Sample.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+List list=new ArrayList();
+list.add(10);
+list.add(20);
+System.out.println(list);
+	}
+
+}
